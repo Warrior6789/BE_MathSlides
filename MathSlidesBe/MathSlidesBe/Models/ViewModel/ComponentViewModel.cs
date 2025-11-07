6b8126761e1bc7406780ff32135ddf1f684e6b6b
philong <philong68687879@gmail.com>
2025-11-07 20:24:04 +0700

    add viewModel

---
+++ MathSlidesBe/MathSlidesBe/Models/ViewModel/ComponentViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MathSlidesBe.Models.ViewModel
+{
+    public class ComponentViewModel
+    {
+        public Guid? Id { get; set; }
+        public Guid? SlideID { get; set; }
+        public string ComponentType { get; set; }
+        public ComponentPropertiesViewModel? Properties { get; set; }
+        public int ZIndex { get; set; }
+    }
+
+}
