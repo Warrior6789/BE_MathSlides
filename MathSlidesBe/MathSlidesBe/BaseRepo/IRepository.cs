b9fd2961af8fdf6d8e4a35cf58aba1bb17dd5497
philong <philong68687879@gmail.com>
2025-11-07 14:01:52 +0700

    update repository

--- MathSlidesBe/MathSlidesBe/BaseRepo/IRepository.cs
+++ MathSlidesBe/MathSlidesBe/BaseRepo/IRepository.cs
@@ -11,5 +11,11 @@ namespace MathSlidesBe.BaseRepo
         Task DeleteAsync(Guid id);
         IQueryable<TEntity> Query(Expression<Func<TEntity, bool>> predicate);
         Task<PagedResult<TEntity>> GetPagedAsync(int pageIndex, int pageSize);
+        Task<TEntity> GetByIdWithIncludesAsync(Guid id, params Expression<Func<TEntity, object>>[] includes);
+        Task<TEntity> FindAsync(Guid id);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        Task RemoveRangeAsync(IEnumerable<TEntity> entites);
+        Task<T> ExcuteInTransactionAsync<T>(Func<Task<T>> action);
+        Task SaveChangeAsync();
     }
 }
