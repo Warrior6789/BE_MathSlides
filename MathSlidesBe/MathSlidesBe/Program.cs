c9d31ab687148dfa0533c494bea97417282b0976
philong <philong68687879@gmail.com>
2025-11-09 22:40:38 +0700

    update program

--- MathSlidesBe/MathSlidesBe/Program.cs
+++ MathSlidesBe/MathSlidesBe/Program.cs
@@ -1,6 +1,6 @@
-
-using MathSlidesBe.BaseRepo;
+﻿using MathSlidesBe.BaseRepo;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 namespace MathSlidesBe
 {
@@ -12,45 +12,63 @@ namespace MathSlidesBe
 
             // Add services to the container.
             builder.Services.AddAuthorization();
-            //);
             builder.Services.AddDbContext<MathSlidesDbContext>();
-            builder.Services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
             builder.Services.AddControllers()
                 .AddJsonOptions(options =>
                 {
                     options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
                 });
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
             builder.Services.AddAuthentication("MyCookieAuth")
-                .AddCookie("MyCookieAuth",options =>
+                .AddCookie("MyCookieAuth", options =>
                 {
                     options.Cookie.Name = "myapp_auth";
                     options.Cookie.HttpOnly = true;
-                    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-                    options.Cookie.SameSite = SameSiteMode.None;
+                    options.Cookie.SecurePolicy = CookieSecurePolicy.None; 
+                    options.Cookie.SameSite = SameSiteMode.Lax; 
                     options.LoginPath = "/auth/login";
                     options.AccessDeniedPath = "/auth/denied";
                     options.SlidingExpiration = true;
                     options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
                 });
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigins", policy =>
+                {
+                    policy
+                        .WithOrigins("http://localhost:5173", "http://localhost:3000", "https://yourdomain.com")
+                        .AllowCredentials()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), "Upload")),
+                RequestPath = "/Upload"
+            });
+
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
 
+            app.UseCors("AllowSpecificOrigins"); 
             app.UseAuthentication();
             app.UseAuthorization();
-
             app.MapControllers();
-
 
             app.Run();
         }
